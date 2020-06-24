2d0483850812feac5747b33cb4a0f1b250ad2e48
HunJeong <junjw123@gmail.com>
2020-06-25 00:56:24 +0900

    :tada: Init spring project

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+
+    kotlin("plugin.spring") version "1.3.72"
+    kotlin("plugin.allopen") version "1.3.72"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    compile("org.springframework.boot", "spring-boot-starter-web", "2.3.1.RELEASE")
+
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
