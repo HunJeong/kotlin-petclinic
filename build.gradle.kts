2d1a7884838bb3e58734a4caf942f86d5a893ee2
HunJeong <junjw123@gmail.com>
2020-06-26 00:05:53 +0900

    :sparkles: db connector, migration tool 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
 
     kotlin("plugin.spring") version "1.3.72"
     kotlin("plugin.allopen") version "1.3.72"
+
+    id("org.flywaydb.flyway") version "6.4.4"
 }
 
 group = "org.example"
@@ -21,6 +23,8 @@ dependencies {
 
     implementation(springBootStarter("web", "2.3.1.RELEASE"))
 
+    implementation("mysql", "mysql-connector-java", "8.0.20")
+
     testImplementation(springBootStarter("test", "2.3.1.RELEASE"))
 }
 
@@ -34,4 +38,10 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
+
+flyway {
+    url = System.getenv("DB_URL")
+    user = System.getenv("DB_USERNAME")
+    password = System.getenv("DB_PASSWORD")
 }
