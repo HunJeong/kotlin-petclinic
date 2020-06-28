ef81c065d13a2a97e971b5888f0a384cc04f36f1
HunJeong <junjw123@gmail.com>
2020-06-28 16:53:43 +0900

    :recycle: @Controller => @RestController

--- src/main/kotlin/org/hooney/petclinic/api/v1/HelloWorldController.kt
+++ src/main/kotlin/org/hooney/petclinic/api/v1/HelloWorldController.kt
@@ -1,15 +1,13 @@
 package org.hooney.petclinic.api.v1
 
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class HelloWorldController {
 
     @GetMapping("/api/v1/hello_world")
-    @ResponseBody
     fun helloWorld(): ResponseEntity<String> = ResponseEntity.ok("Hello World")
 
 }
