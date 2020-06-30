e07509ac78f6be6274f8473e2093c11bae923409
HunJeong <junjw123@gmail.com>
2020-07-01 00:36:43 +0900

    :recycle: content-type 하드코딩된거 변경

--- src/main/kotlin/org/hooney/petclinic/api/v1/VetController.kt
+++ src/main/kotlin/org/hooney/petclinic/api/v1/VetController.kt
@@ -2,13 +2,14 @@ package org.hooney.petclinic.api.v1
 
 import org.hooney.petclinic.entity.Vet
 import org.hooney.petclinic.repository.VetRepository
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class VetController(val vetRepository: VetRepository) {
 
-    @GetMapping("/api/v1/vets", produces = ["application/json"])
+    @GetMapping("/api/v1/vets", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getVets(): List<Vet> = this.vetRepository.findAll()
 
 }
